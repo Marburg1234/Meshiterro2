8563c1b65e0626ccd956377f9c73061c2d65415b
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-07-09 13:14:03 +0000

    [finish] 8-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,8 +1,24 @@
 class PostImagesController < ApplicationController
 
-
+  # 投稿ページを作成
   def new
+    @post_image = PostImage.new
+    #インスタンス変数 = モデル名.new
+    # newメソッド内に記述する内容=注意すること
+  end
 
+  # 投稿データの保存機能を追加
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    # @post_image.user_id = [モデル名].[カラム名]をすることで、
+    # 保存するカラムの中身を指定できる
+    # current_user.id=ログイン中のユーザー情報を取得する記述or表示する
+    # current_user.name : ログイン中のユーザーの名前を表示
+    # current_user.email : ログイン中のユーザーのメールアドレスを表示
+    # current~ はdeviseのヘルパーメソッドである。
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -12,5 +28,14 @@ class PostImagesController < ApplicationController
   def show
 
   end
+
+
+  #ストロングパラメーターを追加する
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+
 
 end
