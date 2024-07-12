6e6b1ea17d22398287b9b1e1be0fe99a4ffb800c
Marburg1234 <172342315+Marburg1234@users.noreply.github.com>
2024-07-12 08:30:23 +0000

    [Finish] -26

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+
   def show
     # User.find(params[:id])にてユーザを絞りこみ⇒対象IDのユーザーを@userに代入する
     # UserというモデルからこのID番号(実際にURLのリクエストで分かる)を取得する
@@ -9,13 +11,13 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params["id"])
+    @user = User.find(params[:id])
   end
 
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path
+    redirect_to user_path(@user.id)
   end
 
 
@@ -24,5 +26,14 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  # ここから追加
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to post_images_path
+    end
+  end
+  # ここまで追加
 
 end
